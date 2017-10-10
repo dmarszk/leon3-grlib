31cb1343d7536a2bfeec07abe3b2de6df2175864
Dominik Marszk <dominik.marszk@esa.int>
2017-10-10 19:12:33 +0000

    Improved hello_world code quality

--- designs/leon3-criticallink-5csx/hello_world.c
+++ designs/leon3-criticallink-5csx/hello_world.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #define APB_UART0_BASE (0x80000100)
 
@@ -12,16 +13,22 @@
 #define UART0_BAUD_RATE (115200)
 #define UART0_SCALER_VALUE ((MAIN_CLOCK + UART0_BAUD_RATE * 4) / (UART0_BAUD_RATE * 8))
 
-int main()
+void delay_loop(int n)
 {
-	int i, j;
-	*(unsigned long*)(APB_UART0_SCALER) = UART0_SCALER_VALUE;
-	*(unsigned long*)(APB_UART0_CTRL) = 0x80000003; // Enable FIFO, TX and RX
-	for(i = 0; i < 10; ++i)
-	{
-		printf("Hello World\n");
-		for(j = 0; j < 100000; ++j)
-			__asm("nop");
-	}
-	return 0;
+  int i;
+  for(i = 0; i < n; ++i)
+    __asm("nop");
+}
+
+int main(void)
+{
+  int i;
+  *(uint32_t*)(APB_UART0_SCALER) = UART0_SCALER_VALUE;
+  *(uint32_t*)(APB_UART0_CTRL) = 0x80000003; // Enable FIFO, TX and RX
+  for(i = 0; i < 10; ++i)
+  {
+    printf("Hello World %d\n", i);
+    delay_loop(500000);
+  }
+  return 0;
 }
